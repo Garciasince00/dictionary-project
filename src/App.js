1e79e2334ab8ca5c3dff79410b2b4e3bff180bce
Garciasince00 <estelagarcia1114@gmail.com>
2023-09-14 11:45:37 +0300

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a href='#' className='btn btn-primary'>Hello</a>
-       Hello
-      </header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This project was coded by Sandra Garcia and is open sourced
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
