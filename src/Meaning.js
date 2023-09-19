02b11d1ac85d449e1b74013070d95626c3154c83
Garciasince00 <estelagarcia1114@gmail.com>
2023-09-19 20:37:37 +0300

    created example an synonym components

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,15 @@
 import React from "react";
+import Synonyms from "./Synonyms";
+import Example from "./Example";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>{props.meaning.definition}</p>
-      <p>
-        <em>{props.meaning.example}</em>
+      <p><strong>Definition: </strong>{props.meaning.definition}
+      <br />
+      <Example example={props.meaning.example}/>
+        <Synonyms synonyms={props.meaning.synonyms} />
       </p>
     </div>
   );
